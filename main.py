e9d40a654da548ecf46b73fa22b247e7bd98471d
MaksimKharlamov <MaksimKharlamov35@yandex.ru>
2024-02-17 21:10:32 +0300

    2

--- main.py
+++ main.py
@@ -9,5 +9,12 @@ def index(title):
     return render_template("base.html", title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    if 'инженер' in prof or 'строитель' in prof:
+        return render_template('engineer.html')
+    return render_template('science.html')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
