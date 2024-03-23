90696904591ca94404a652122714d437fc6138cd
MaksimKharlamov <MaksimKharlamov35@yandex.ru>
2024-03-23 17:38:51 +0300

    sqlalchemy #4

---
+++ data/jobform.py
@@ -0,0 +1,12 @@
+from flask_wtf import *
+from wtforms import *
+from wtforms.validators import *
+
+
+class LoginForm(FlaskForm):
+    job = StringField('Job Title', validators=[DataRequired()])
+    team_leader = IntegerField('Team Leader id', validators=[DataRequired()])
+    work_size = IntegerField('Work Size', validators=[DataRequired()])
+    collaborators = StringField('Collaborators', validators=[DataRequired()])
+    is_finished = BooleanField('Is job finished?')
+    submit = SubmitField('Submit')
